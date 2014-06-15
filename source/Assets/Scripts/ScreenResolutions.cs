f4cb4c6cdf88225e001b1a0e952d542a737f5913
João Kucera <hi@joaokucera.com>
2014-06-15 00:00:56 -0300

    resolution
    changing resolution

---
+++ source/Assets/Scripts/ScreenResolutions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResolutions : MonoBehaviour {
+	
+	void Awake () 
+	{
+		print (Camera.main.aspect);
+
+		// 4:3
+		if (Camera.main.aspect > 1.3f && Camera.main.aspect < 1.4f)
+		{
+			Camera.main.orthographicSize = 15.36f;
+		}
+		// 3:2
+		else if (Camera.main.aspect == 1.5f)
+		{
+			Camera.main.orthographicSize = 13.66f;
+		}
+		// 16:9
+		else if (Camera.main.aspect == 16/9)
+		{
+			Camera.main.orthographicSize = 11.53f;
+		}
+		// 
+		else if (Camera.main.aspect == 16/10)
+		{
+			Camera.main.orthographicSize = 12.81f;
+		}
+		if (Camera.main.aspect == 17/10)
+		{
+			Camera.main.orthographicSize = 12.05f;
+		}
+
+		// 4.3 = 1.333333
+		// 3.2 = 1.5
+		// 16.9 = 1.777778
+		// 16.10 = 1.6
+		// 17.10 = 1.7
+	}
+}
--- source/Assets/Scripts/ShipMovement.cs
+++ source/Assets/Scripts/ShipMovement.cs
@@ -12,61 +12,84 @@ public class ShipMovement : MonoBehaviour {
 
 	public float speed;
 
-	private SpriteRenderer spriteRenderer;
 	private MovementSide movementSide = MovementSide.NONE;
-	private bool enableLeftButton = true;
-	private bool enableRightButton = true;
-	private float yStartPosition;
+	private Camera mainCamera;
+	private SpriteRenderer spriteRenderer;
+	private Vector2 boundSize;
+
+	private float verticalFixedPosition;
 
 	void Start () 
 	{
+		mainCamera = Camera.main;
 		spriteRenderer = (SpriteRenderer)renderer;
+		boundSize = spriteRenderer.sprite.bounds.size / 2;
 
-		Vector2 startPosition = Camera.main.ScreenToWorldPoint (
-			new Vector2 (Screen.width / 2, spriteRenderer.bounds.size.x * 3.5f));
+
+		Vector2 startPosition = //new Vector2((mainCamera.aspect) / 2,
+//		                                    0); 
+//		Debug.Log (startPosition);
+
+		Camera.main.ScreenToWorldPoint (new Vector2 (Screen.width / 2, spriteRenderer.bounds.size.x * 3.5f));
 
 		transform.position = startPosition;
-
-		yStartPosition = startPosition.y;
-	}
-
-	void OnTriggerEnter2D(Collider2D collider)
-	{
-		if (collider.tag == "InvisiblePoint")
-		{
-			movementSide = MovementSide.NONE;
-
-			if (collider.name.Contains("Left"))
-			{
-				enableLeftButton = false;
-			}
-			else if (collider.name.Contains("Right"))
-			{
-				enableRightButton = false;
-			}
-		}
+		verticalFixedPosition = startPosition.y;
 	}
 
 	void Update () 
 	{
 		transform.TranslateTo ((int)movementSide * speed, 0, 0, Time.deltaTime);
 
-		transform.position = new Vector2 (transform.position.x, yStartPosition);
+		transform.position = new Vector2 (transform.position.x, verticalFixedPosition);
+
+		// Enforce ship inside the screen.
+		EnforceBounds ();
 	}
 
 	void OnGUI()
 	{
-		if (enableLeftButton && GUI.Button(new Rect(0, Screen.height - 40, 40, 40), "<<"))
+		if (GUI.Button(new Rect(0, Screen.height - 40, 40, 40), "<<"))
 		{
 			movementSide = MovementSide.LEFT;
-
-			enableRightButton = true;
 		}
-		else if (enableRightButton && GUI.Button(new Rect(Screen.width - 40, Screen.height - 40, 40, 40), ">>"))
+		else if (GUI.Button(new Rect(Screen.width - 40, Screen.height - 40, 40, 40), ">>"))
 		{
 			movementSide = MovementSide.RIGHT;
-
-			enableLeftButton = true;
 		}
+	}
+
+	/// <summary>
+	/// Enforces the bounds.
+	/// </summary>
+	private void EnforceBounds ()
+	{
+		// Current positions.
+		Vector2 currentPosition = transform.position;
+		Vector2 currentCameraPosition = mainCamera.transform.position;
+
+		// Get X distances.
+		float xDistance = mainCamera.aspect * mainCamera.orthographicSize;
+		float xMax = currentCameraPosition.x + xDistance - boundSize.x;
+		float xMin = currentCameraPosition.x - xDistance + boundSize.x;
+
+		// Fix vertical bounds
+		if (currentPosition.x < xMin || currentPosition.x > xMax)
+		{
+			currentPosition.x = Mathf.Clamp(currentPosition.x, xMin, xMax);
+		}
+
+		// Get Y distances.
+		float yDistance = mainCamera.orthographicSize;
+		float yMax = currentCameraPosition.y + yDistance;
+		float yMin = currentCameraPosition.y - yDistance;
+		
+		// Fix vertical bounds
+		if (currentPosition.y < yMin || currentPosition.y > yMax)
+		{
+			currentPosition.y = Mathf.Clamp(currentPosition.y, yMin, yMax);
+		}
+
+		// Set position.
+		transform.position = currentPosition;
 	}
 }
